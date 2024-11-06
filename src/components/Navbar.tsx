08a17e5c265ece2cf8ba35c6774e4cf8c04880e1
12coqui <coquiserra1992@gmail.com>
2024-11-06 15:56:48 -0300

    Fixing formatting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,33 @@
-import { Link } from 'react-router-dom';
-import styles from '../styles/Navbar.module.css';
-import { List, PlusSquare } from 'react-feather';
-function Navbar() {
-	return (
-		<nav className={styles.navbar}>
-			<h1>Greengrocery</h1>
-			<ul className={styles.navList}>
-				<li className={styles.navItem}>
-					<Link to='/' className={styles.navLink}>
-						<span>
-							<List />
-						</span>
+import { Link } from "react-router-dom";
+import { List, PlusSquare } from "react-feather";
 
-						<span>List</span>
-					</Link>
-				</li>
-				<li className={styles.navItem}>
-					<Link to='/form' className={styles.navLink}>
-						<span>
-							<PlusSquare />
-						</span>
-						<span>New Order</span>
-					</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+import styles from "../styles/Navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <h1>Greengrocery</h1>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link to="/" className={styles.navLink}>
+            <span>
+              <List />
+            </span>
+
+            <span>List</span>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/form" className={styles.navLink}>
+            <span>
+              <PlusSquare />
+            </span>
+            <span>New Order</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -1,73 +1,84 @@
-import { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Trash2, Edit } from 'react-feather';
-import styles from '../styles/OrderView.module.css';
-import VisuallyHidden from './VisuallyHidden';
-import { OrderContext } from './context/OrdersContext';
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Trash2, Edit } from "react-feather";
+import VisuallyHidden from "../helpers/VisuallyHidden";
+import { OrderContext } from "./context/OrdersContext";
+import styles from "../styles/OrderView.module.css";
 
 interface OrderCardProps {
-	customerName: string;
-	item: string;
-	quantity: number;
-	status: string;
-	date: string;
-	time: string;
-	id: string;
+  customerName: string;
+  item: string;
+  quantity: number;
+  status: string;
+  date: string;
+  time: string;
+  id: string;
 }
 
-function OrderCard({ customerName, item, quantity, status, date, time, id }: OrderCardProps) {
-	const [show, setShow] = useState(false);
-	const { orders, setOrders } = useContext(OrderContext);
-	const navigate = useNavigate();
+function OrderCard({
+  customerName,
+  item,
+  quantity,
+  status,
+  date,
+  time,
+  id,
+}: OrderCardProps) {
+  const [show, setShow] = useState(false);
+  const { orders, setOrders } = useContext(OrderContext);
+  const navigate = useNavigate();
 
-	function handleEdit(id: string) {
-		navigate(`/form?id=${id}`);
-	}
+  function handleEdit(id: string) {
+    navigate(`/form?id=${id}`);
+  }
 
-	function handleDelete(id: string) {
-		const updatedOrders = orders.filter((order: any) => order.id !== id);
-		setOrders(updatedOrders);
-	}
+  function handleDelete(id: string) {
+    const updatedOrders = orders.filter((order: any) => order.id !== id);
+    setOrders(updatedOrders);
+  }
 
-	return (
-		<li className={styles.item} style={{ border: `var(--${status}) solid 2px` }}>
-			<div>
-				<span>Customer Name:</span> <span>{customerName}.</span>
-			</div>
-			<div>
-				<span>Item: </span>
-				<span>{item}.</span>
-			</div>
-			<div>Quantity: {quantity}.</div>
-			<div>
-				<span>Status: </span>
-				<span>{status}.</span>
-			</div>
-			{show && (
-				<>
-					<div>
-						<span>Date: </span>
-						<span>{date}.</span>
-					</div>
-					<div>
-						<span>Time: </span>
-						<span>{time}.</span>
-					</div>
-				</>
-			)}
-			<button className={styles.show} onClick={() => setShow(prev => !prev)}>
-				{show ? 'Show less' : 'Show more'}
-			</button>
-			<button className={styles.delete} onClick={() => handleDelete(id)}>
-				<Trash2 size={18} />
-				<VisuallyHidden>Delete item</VisuallyHidden>
-			</button>
-			<button className={styles.edit} onClick={() => handleEdit(id)}>
-				<Edit size={16} />
-				<VisuallyHidden>Edit item</VisuallyHidden>
-			</button>
-		</li>
-	);
+  return (
+    <li
+      className={styles.item}
+      style={{ border: `var(--${status}) solid 2px` }}
+    >
+      <div>
+        <span>Customer Name:</span> <span>{customerName}.</span>
+      </div>
+      <div>
+        <span>Item: </span>
+        <span>{item}.</span>
+      </div>
+      <div>Quantity: {quantity}.</div>
+      <div>
+        <span>Status: </span>
+        <span>{status}.</span>
+      </div>
+      {show && (
+        <>
+          <div>
+            <span>Date: </span>
+            <span>{date}.</span>
+          </div>
+          <div>
+            <span>Time: </span>
+            <span>{time}.</span>
+          </div>
+        </>
+      )}
+      <button className={styles.show} onClick={() => setShow((prev) => !prev)}>
+        {show ? "Show less" : "Show more"}
+      </button>
+      <button className={styles.delete} onClick={() => handleDelete(id)}>
+        <Trash2 size={18} />
+        <VisuallyHidden>Delete item</VisuallyHidden>
+      </button>
+      <button className={styles.edit} onClick={() => handleEdit(id)}>
+        <Edit size={16} />
+        <VisuallyHidden>Edit item</VisuallyHidden>
+      </button>
+    </li>
+  );
 }
 
 export default OrderCard;
